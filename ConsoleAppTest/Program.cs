6cc4650d192b4cab679a5920c3c73ac1137932cc
nik-rimor <nickkekes@gmail.com>
2020-12-30 20:57:24 +0200

    First Init with 2 views

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using SimpleTrader.EntityFramework;
+using SimpleTrader.EntityFramework.Services;
+using System;
+using System.Linq;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataService<User> userService = new GenericDataService<User>(new SimpleTraderDbContextFactory());
+            Console.WriteLine(userService.GetAll().Result.Count());
+            //userService.Create(new User { Username = "Test2" }).Wait();
+            //Console.WriteLine(userService.Get(1).Result);
+            //Console.WriteLine(userService.Update(2, new User { Username = "Testupdate" }).Result);
+            Console.WriteLine(userService.Delete(2).Result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleTrader.Domain/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.Domain.Models
+{
+    public class Account: DomainObject
+    {
+        public User AccountHolder { get; set; }
+        public double Balance { get; set; }
+        public IEnumerable<AssetTransaction> AssetTransactions { get; set; }
+
+
+    }
+}
